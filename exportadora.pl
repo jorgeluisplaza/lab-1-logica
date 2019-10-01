614281be5d1969afaa2f0a52c2602c1abea511d1
Jorge <jorge.plaza@usach.cl>
2019-10-01 12:16:24 -0300

    Subiendo lab

---
+++ exportadora.pl
@@ -0,0 +1,37 @@
+ 
+coloracion(negra).
+coloracion(roja).
+coloracion(blanca).
+coloracion(rosada).
+
+cobertura(alto).
+cobertura(medio).
+cobertura(bajo).
+cobertura(ninguno).
+
+calibre(18).
+calibre(19).
+calibre(20).
+calibre(21).
+calibre(22).
+calibre(23).
+calibre(24).
+calibre(25).
+calibre(26).
+calibre(27).
+calibre(28).
+calibre(29).
+calibre(30).
+calibre(31).
+calibre(32).
+
+defecto(blanca, 0, 0, 0, "Sin color").		
+
+%fruta(Cicatrices, Arrugas, Cobertura, Coloracion)
+
+sinColor(Coloracion):-
+	coloracion(Coloracion).
+
+
+
+
